8c8daff700f5b19e826e860925cc5cd9bdbdc04e
mrkulk <tejasdkulkarni@gmail.com>
2015-10-17 00:40:42 -0400

    clean up

--- main.lua
+++ main.lua
@@ -9,35 +9,33 @@ require 'xlua'
 require 'Base'
 require 'optim'
 require 'image'
-trainLogger = optim.Logger(paths.concat('logs/', 'train.log'))
-testLogger = optim.Logger(paths.concat('logs/', 'test.log'))
-
-params = {
-	bsize = 120,
-	image_width = 32,
-	template_width = 10,
-	num_acrs = 9,
-	rnn_size = 100,
-	seq_length=1,
-	layers=2,
-	decay=2,
-	dropout=0,
-	init_weight=0.1,
-	lr=5e-3,
-	max_epochs=50,
-	max_grad_norm=5
-}
-
+require 'sys'
+require 'pl'
+params = lapp[[
+   -s,--save          (default "logs")      subdirectory to save logs
+   -m,--model         (default "convnet")   type of model tor train: convnet | mlp | linear
+   -p,--plot                                plot while training
+   -r,--lr  					(default 0.005)				learning rate
+   -i,--max_epochs    (default 50)           maximum nb of iterations per batch, for LBFGS
+   --bsize            (default 120)           bsize
+   --image_width			(default 32)           
+   --template_width		(default 10)           
+   --num_acrs           (default 3)           number of acrs
+   --rnn_size					(default 100)
+   --seq_length				(default 1)
+   --layers						(default 1)
+   --init_weight			(default 0.1)
+   --max_grad_norm 		(default 5)
+]]
 config = {
     learningRate = params.lr,
     momentumDecay = 0.1,
     updateDecay = 0.01
 }
-
 require 'model'
-
-
 -- torch.manualSeed(1)
+trainLogger = optim.Logger(paths.concat(params.save .. '/', 'train.log'))
+testLogger = optim.Logger(paths.concat(params.save .. '/', 'test.log'))
 
 -- create training set and normalize
 trainData = mnist.loadTrainSet(nbTrainingPatches, geometry)
@@ -47,6 +45,7 @@ trainData:normalizeGlobal(mean, std)
 testData = mnist.loadTestSet(nbTestingPatches, geometry)
 testData:normalizeGlobal(mean, std)
 
+
 local function unit_test()
 	model = create_network(params)
 	model:cuda()
@@ -54,7 +53,6 @@ local function unit_test()
 	    {torch.zeros(params.bsize, params.rnn_size):cuda(), torch.zeros(params.bsize, params.rnn_size):cuda(), torch.zeros(params.bsize, params.rnn_size):cuda(), torch.zeros(params.bsize, params.rnn_size):cuda() },
 		})
 	print(ret[3])
-
 	ret = model:backward({torch.rand(params.bsize, 1, 32, 32):cuda(),
 	    {torch.zeros(params.bsize, params.rnn_size):cuda(), torch.zeros(params.bsize, params.rnn_size):cuda(), torch.zeros(params.bsize, params.rnn_size):cuda(), torch.zeros(params.bsize, params.rnn_size):cuda() },
 		},
@@ -63,13 +61,27 @@ local function unit_test()
 		{torch.zeros(params.bsize, params.rnn_size):cuda(), torch.zeros(params.bsize, params.rnn_size):cuda(), torch.zeros(params.bsize, params.rnn_size):cuda(), torch.zeros(params.bsize, params.rnn_size):cuda() }
 		,torch.rand(params.bsize, 1, 32, 32):cuda()
 		})
-	-- image.save('test.png',ret[2])
 end
 -- unit_test()
 
 entities = setup()
 
-function main()
+function get_batch(t, data)
+  local inputs = torch.Tensor(params.bsize,1,32,32)
+  local k = 1
+  for i = t,math.min(t+params.bsize-1,data:size()) do
+     -- load new sample
+     local sample = data[i]
+     local input = sample[1]:clone()
+     local _,target = sample[2]:clone():max(1)
+     inputs[{k,1,{},{}}] = input
+     k = k + 1
+  end
+  inputs = inputs:cuda()
+  return inputs
+end
+
+function init()
   print("Network parameters:")
   print(params)
   reset_state(state)
@@ -78,71 +90,50 @@ function main()
   local start_time = torch.tic()
   print("Starting training.")
   print(trainData:size())
-  
+end
+
+function train()
   for epc = 1,params.max_epochs do
   	print('EPOCH:', epc)
   	local cntr = 0
 		for t = 1,trainData:size(),params.bsize do
 			xlua.progress(t, trainData:size())
 	    -- create mini batch
-	    local inputs = torch.Tensor(params.bsize,1,32,32)
-	    local k = 1
-	    for i = t,math.min(t+params.bsize-1,trainData:size()) do
-	       -- load new sample
-	       local sample = trainData[i]
-	       local input = sample[1]:clone()
-	       local _,target = sample[2]:clone():max(1)
-	       inputs[{k,1,{},{}}] = input
-	       k = k + 1
-	    end
-	    inputs = inputs:cuda()
+	    local inputs = get_batch(t, trainData)
 			local perp, output = fp(inputs)
 			bp(inputs)
 			cutorch.synchronize()
 			collectgarbage()	
 
 			if math.fmod(cntr, 1) == 0 then
-				-- testing
-				for tt = 1,1 do--trainData:size(),params.bsize do
-					local inputs = torch.Tensor(params.bsize,1,32,32)
-			    local k = 1
-			    for ii = tt,math.min(tt+params.bsize-1,testData:size()) do
-			       -- load new sample
-			       local sample = testData[ii]
-			       local input = sample[1]:clone()
-			       local _,target = sample[2]:clone():max(1)
-			       inputs[{k,1,{},{}}] = input
-			       k = k + 1
-			    end
-			    inputs = inputs:cuda()
-					local test_perp, test_output = fp(inputs)
-
-					-- image.save('pred.png', test_output[1][1])
-					-- image.save('target.png', inputs[1][1])
-		      window1=image.display({image=test_output, nrow=6, legend='Predictions, step : '.. cntr, win=window1})
-		      window2=image.display({image=inputs, nrow=6, legend='Targets, step : '.. cntr, win=window2})
-
-		      local part_images = {}
-		      for pp = 1,params.num_acrs do
-		      	local p1_images = entities[pp].data.module.bias[1]:reshape(params.template_width, params.template_width)
-		      	part_images[pp] = p1_images
-		      end
-		      window3 = image.display({image=part_images, nrow=3, legend='Strokes, step:' .. cntr, win=window3})
-		      -- print(test_output)
-		      -- exit()
-
-					testLogger:add{['% perp (test set)'] =  test_perp}
-					testLogger:style{['% perp (test set)'] = '-'}
-					testLogger:plot()
-				end
+				test()
 			end
 
 			cntr = cntr + 1
-			-- trainLogger:add{['% perp (train set)'] =  perp}
-			-- trainLogger:style{['% perp (train set)'] = '-'}
-			-- trainLogger:plot()
+			trainLogger:add{['% perp (train set)'] =  perp}
+			trainLogger:style{['% perp (train set)'] = '-'}
 		end
 	end
 end
 
-main()
+function test()
+	-- testing
+	for tt = 1,1 do--trainData:size(),params.bsize do
+		local inputs = get_batch(tt, testData)
+		local test_perp, test_output = fp(inputs)
+    window1=image.display({image=test_output, nrow=6, legend='Predictions', win=window1})
+    window2=image.display({image=inputs, nrow=6, legend='Targets', win=window2})
+    local part_images = {}
+    for pp = 1,params.num_acrs do
+    	local p1_images = entities[pp].data.module.bias[1]:reshape(params.template_width, params.template_width)
+    	part_images[pp] = p1_images
+    end
+    window3 = image.display({image=part_images, nrow=3, legend='Strokes', win=window3})
+		testLogger:add{['% perp (test set)'] =  test_perp}
+		testLogger:style{['% perp (test set)'] = '-'}
+		testLogger:plot()
+	end
+end
+
+init()
+train()
